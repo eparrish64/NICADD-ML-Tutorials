b3d1e8e0192be02ee231af8a2493e83ea4e4943c
Elliot Parrish <elliot.wesley.parrish.cern.ch>
2019-04-24 16:15:48 -0500

    Adjusting setup.sh and setup_environment.sh to allow setup_environment.sh to stand alone as script to call when reconnecting.

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+conda activate mlTutorials_gpu;
 export PYTHIA8=/xdata/epaarrish/mlhep2018/pythia8240;
 export PYTHIA_INCLUDE=/xdata/eparrish/mlhep2018/pythia8240/include;
 export PYTHIA_LIB=/xdata/eparrish/mlhep2018/pythia8240/lib;
