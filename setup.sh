07a27b802b412719a18149d0864a96a1b13cb3a8
Elliot Parrish <42135977+eparrish64@users.noreply.github.com>
2019-04-26 14:53:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,10 @@
 ## Date:4/24/2019
 
 cd /xdata/$USER;
-echo "Downloading Anaconda installation script";
-curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh;
-echo "Installing Anaconda, be patient, this will take some time";
+echo "Downloading and installing Anaconda installation script, this will take some time";
 echo "When asked, accept the user agreement";
 echo "Be sure to change install location to /xdata/USERNAME/anaconda3";
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh;
 bash Anaconda3-2019.03-Linux-x86_64.sh;
 source ~/.bashrc;
 conda config --set auto_activate_base false;
