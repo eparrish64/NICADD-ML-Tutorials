1d79ff4716c0a4dae93ab28fab513351a50ac182
Elliot Parrish <elliot.wesley.parrish.cern.ch>
2019-05-08 18:46:44 -0500

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 ## Author: Elliot Parrish
 ## Date:4/24/2019
 
-cd /xdata/$USER;
-echo "Downloading Anaconda installation script";
-curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh;
-echo "Installing Anaconda, be patient, this will take some time";
-echo "When asked, accept the user agreement";
-echo "Be sure to change install location to /xdata/USERNAME/anaconda3";
-bash Anaconda3-2019.03-Linux-x86_64.sh;
-source ~/.bashrc;
-conda config --set auto_activate_base false;
+# cd /xdata/$USER;
+# echo "Downloading Anaconda installation script";
+# curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh;
+# echo "Installing Anaconda, be patient, this will take some time";
+# echo "When asked, accept the user agreement";
+# echo "Be sure to change install location to /xdata/USERNAME/anaconda3";
+# bash Anaconda3-2019.03-Linux-x86_64.sh;
 # source ~/.bashrc;
-cd /xdata/$USER/NICADD-ML-Tutorials;
-conda env create -f mlTutorials_gpu.yml;
+# conda config --set auto_activate_base false;
+# source ~/.bashrc;
+# cd /xdata/$USER/NICADD-ML-Tutorials;
+# conda env create -f mlTutorials_gpu.yml;
 conda activate mlTutorials_gpu;
 
 pip install cython pydot;
