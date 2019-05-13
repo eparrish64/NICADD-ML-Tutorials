1da76d6f34f13b8a808dee784a67f0836dd3eaa5
Elliot Parrish <elliot.wesley.parrish.cern.ch>
2019-05-12 22:12:05 -0500

    Adding DecisionTrees jupyter notebook

--- setup.sh
+++ setup.sh
@@ -3,42 +3,26 @@
 ## Author: Elliot Parrish
 ## Date:4/24/2019
 
-<<<<<<< HEAD
-# cd /xdata/$USER;
-# echo "Downloading Anaconda installation script";
-# curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh;
-# echo "Installing Anaconda, be patient, this will take some time";
-# echo "When asked, accept the user agreement";
-# echo "Be sure to change install location to /xdata/USERNAME/anaconda3";
-# bash Anaconda3-2019.03-Linux-x86_64.sh;
-# source ~/.bashrc;
-# conda config --set auto_activate_base false;
-# source ~/.bashrc;
-# cd /xdata/$USER/NICADD-ML-Tutorials;
-# conda env create -f mlTutorials_gpu.yml;
-=======
 cd /xdata/$USER;
 echo "Downloading and installing Anaconda installation script, this will take some time";
 echo "When asked, accept the user agreement";
 echo "Be sure to change install location to /xdata/USERNAME/anaconda3";
-# curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh;
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash Anaconda3-2019.03-Linux-x86_64.sh;
-bash Miniconda3-latest-Linux-x86_65.sh;
-source ~/.bashrc;
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh &&
+bash Miniconda3-latest-Linux-x86_65.sh && 
+source ~/.bashrc &&
 conda config --set auto_activate_base false;
-# source ~/.bashrc;
 cd -;
-conda env create -f mlTutorials_gpu.yml;
->>>>>>> 9146a0f432e874724436cae5dbfd39ef9bb15241
-conda activate mlTutorials_gpu;
+conda env create -f mlTutorials_gpu.yml &&
 
-pip install cython pydot;
+conda activate mlTutorials_gpu &&
+
+pip install cython pydot &&
 # source setup_environment.sh;
-export PYTHIA8=/xdata/epaarrish/pythia8240;
-export PYTHIA_INCLUDE=/xdata/eparrish/pythia8240/include;
-export PYTHIA_LIB=/xdata/eparrish/pythia8240/lib;
-export LD_LIBRARY_PATH=/xdata/eparrish/pythia8240/lib:$LD_LIBRARY_PATH;
+export PYTHIA8=/xdata/epaarrish/pythia8240 &&
+export PYTHIA_INCLUDE=/xdata/eparrish/pythia8240/include &&
+export PYTHIA_LIB=/xdata/eparrish/pythia8240/lib &&
+export LD_LIBRARY_PATH=/xdata/eparrish/pythia8240/lib:$LD_LIBRARY_PATH &&
 pip install git+https://github.com/maxim-borisyak/pythia-mill.git@master
 
-# ssh -L localhost:1999:localhost:1999 eparrish@cms1.nicadd.niu.edu
+# jupyter notebook --no-browser --port=NUMBER
+# ssh -L localhost:PORT:localhost:PORT USERNAME@cms1.nicadd.niu.edu
